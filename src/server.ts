7320b4bac5b71b907c6d59aa4423f718a4003b7e
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2025-12-06 12:44:11 +0600

    server setup

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express"; 
+import {Pool} from "pg"; 
+
+const app = express()
+const port = 5000; 
+
+
+//parser
+app.use(express.json()); 
+
+app.get('/', (req : Request, res : Response) => {
+  res.send('Welcome to the vehicle rental server!')
+})
+
+app.post("/", (req : Request, res : Response) => {
+    console.log(req.body);
+
+    res.status(201).json({
+        success: true, 
+        message: "API is working."
+    })
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
