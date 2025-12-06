2090178ff34258972b9e63f4ce7c4c6217126d04
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2025-12-06 21:30:03 +0600

    users and vehicles crud added

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,22 @@
-import express, { Request, Response } from "express"; 
-import {Pool} from "pg"; 
-import dotenv from 'dotenv'; 
-import path from "path"; 
+import express, { Request, Response } from "express";
+import { Pool } from "pg";
+import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config({path: path.join(process.cwd(), '.env')}); 
-const app = express()
-const port = 5000; 
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+const app = express();
+const port = 5000;
 //parser
-app.use(express.json()); 
+app.use(express.json());
 
-// DB 
+// DB
 const pool = new Pool({
-    connectionString: `${process.env.CONNECTION_STR}`
-})
+  connectionString: `${process.env.CONNECTION_STR}`,
+});
 
 const initDB = async () => {
   try {
+    //users
     await pool.query(`CREATE TABLE IF NOT EXISTS users (
       id SERIAL PRIMARY KEY,
       name VARCHAR(100) NOT NULL,
@@ -25,6 +26,7 @@ const initDB = async () => {
       role VARCHAR(20) NOT NULL CHECK (role IN ('admin', 'customer'))
     )`);
 
+    //vehicles
     await pool.query(`CREATE TABLE IF NOT EXISTS vehicles (
       id SERIAL PRIMARY KEY,
       vehicle_name VARCHAR(255) NOT NULL,
@@ -34,6 +36,7 @@ const initDB = async () => {
       availability_status VARCHAR(20) NOT NULL CHECK (availability_status IN ('available', 'booked'))
     )`);
 
+    //bookings
     await pool.query(`CREATE TABLE IF NOT EXISTS bookings (
       id SERIAL PRIMARY KEY,
       customer_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
@@ -51,20 +54,337 @@ const initDB = async () => {
   }
 };
 
-initDB(); 
+initDB();
 
-app.get('/', (req : Request, res : Response) => {
-  res.send('Welcome to the vehicle rental server!')
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to the vehicle rental server!");
+});
 
-app.post("/", (req : Request, res : Response) => {
-    console.log(req.body);
+//users crud
+//create
+app.post("/api/v1/users", async (req: Request, res: Response) => {
+  let { name, email, password, phone, role } = req.body;
+  // console.log(req.body);
+  email = req.body.email.toLowerCase();
+  if (!password || password.length < 6) {
+    return res.status(400).json({
+      success: false,
+      message: "Password must be at least 6 characters long.",
+    });
+  }
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name, email, password, phone, role) VALUES($1, $2, $3, $4, $5) RETURNING *`,
+      [name, email, password, phone, role]
+    );
 
-    res.status(201).json({
+    return res.status(201).json({
+      success: true,
+      message: "User Inserted Successfully!",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+//get
+app.get("/api/v1/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users`);
+
+    res.status(200).json({
+      success: true,
+      message: "Users retrieved successfully!",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+//get single user
+app.get("/api/v1/users/:userId", async (req: Request, res: Response) => {
+  try {
+    // console.log(req.params.userId);
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [
+      req.params.userId,
+    ]);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found!",
+      });
+    }
+    else{
+      res.status(200).json({
         success: true, 
-        message: "API is working."
-    })
-})
+        message: "User fetched successfully!", 
+        data: result.rows[0]
+      })
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+//update single user
+app.put("/api/v1/users/:userId", async (req: Request, res: Response) => 
+  {
+      const { name, email, password, phone, role } = req.body;
+  try {
+   
+    const result = await pool.query(`UPDATE users SET name=$1, email=$2, password=$3, phone=$4, role=$5 WHERE id = $6 RETURNING *`, [name, email, password, phone, role , req.params.userId])
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found!",
+      });
+    }
+    else{
+      res.status(200).json({
+        success: true, 
+        message: "User updated successfully!", 
+        data: result.rows[0]
+      })
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+
+
+//Delete single user
+app.delete("/api/v1/users/:userId", async (req: Request, res: Response) => {
+  const userId = req.params.userId; 
+  try {
+    const bookingCheck = await pool.query(`SELECT * FROM bookings WHERE customer_id = $1 AND status ='active' `, [userId]); 
+
+    if(bookingCheck.rows.length > 0){
+      return res.status(400).json({
+        success: false, 
+        message: "This user can not be deleted because it has active bookings."
+      })
+    }
+
+    const result = await pool.query(`DELETE FROM users WHERE id = $1`, [
+      userId,
+    ]);
+
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found!",
+      });
+    }
+    else{
+      res.status(200).json({
+        success: true, 
+        message: "User deleted successfully!", 
+        data: result.rows
+      })
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+//vehicles CRUD
+//create
+app.post("/api/v1/vehicles", async (req: Request, res: Response) => {
+  let { vehicle_name, type, registration_number, daily_rent_price, availability_status } = req.body;
+  // console.log(req.body);
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO vehicles(vehicle_name, type, registration_number, daily_rent_price, availability_status) VALUES($1, $2, $3, $4, $5) RETURNING *`,
+      [vehicle_name, type, registration_number, daily_rent_price, availability_status]
+    );
+
+    return res.status(201).json({
+      success: true,
+      message: "Vehicle Inserted Successfully!",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+
+//get vehicles
+app.get("/api/v1/vehicles", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM vehicles`);
+
+    res.status(200).json({
+      success: true,
+      message: "vehicles retrieved successfully!",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+//get single vehicle
+app.get("/api/v1/vehicles/:vehicleId", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM vehicles WHERE id = $1`, [
+      req.params.vehicleId,
+    ]);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "Vehicle not found!",
+      });
+    }
+    else{
+      res.status(200).json({
+        success: true, 
+        message: "Vehicle fetched successfully!", 
+        data: result.rows[0]
+      })
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+//update single vehicle
+app.put("/api/v1/vehicles/:vehicleId", async (req: Request, res: Response) => 
+  {
+      const { vehicle_name, type, registration_number, daily_rent_price, availability_status } = req.body;
+
+      
+  try {
+   
+    const result = await pool.query(`UPDATE vehicles SET vehicle_name=$1, type=$2, registration_number=$3, daily_rent_price=$4, availability_status=$5 WHERE id=$6 RETURNING *`, [vehicle_name, type, registration_number, daily_rent_price, availability_status , req.params.vehicleId])
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "Vehicle not found!",
+      });
+    }
+    else{
+      res.status(200).json({
+        success: true, 
+        message: "Vehicle updated successfully!", 
+        data: result.rows[0]
+      })
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+//Delete single vehicle
+app.delete("/api/v1/vehicles/:vehicleId", async (req: Request, res: Response) => {
+  const vehicleId = req.params.vehicleId; 
+  try {
+    const bookingCheck = await pool.query(`SELECT * FROM bookings WHERE vehicle_id = $1 AND status ='active' `, [vehicleId]); 
+
+    if(bookingCheck.rows.length > 0){
+      return res.status(400).json({
+        success: false, 
+        message: "This vehicle can not be deleted because it has active bookings."
+      })
+    }
+
+    const result = await pool.query(`DELETE FROM vehicles WHERE id = $1`, [
+      vehicleId,
+    ]);
+
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "Vehicle not found!",
+      });
+    }
+    else{
+      res.status(200).json({
+        success: true, 
+        message: "Vehicle deleted successfully!", 
+        data: result.rows
+      })
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+
+//Bookings CRUD
+
+
+//create booking
+app.post("/api/v1/bookings", async (req: Request, res: Response) => {
+  let {  customer_id, vehicle_id, rent_start_date, rent_end_date, total_price } = req.body;
+  // console.log(req.body);
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO bookings(customer_id, vehicle_id, rent_start_date, rent_end_date, total_price) VALUES($1, $2, $3, $4, $5) RETURNING *`,
+      [customer_id, vehicle_id, rent_start_date, rent_end_date, total_price]
+    );
+
+    return res.status(201).json({
+      success: true,
+      message: "Booking Inserted Successfully!",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+
+
+
+
+
+
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
